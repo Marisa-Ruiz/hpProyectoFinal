daf44d1481a119520362b8a37b20c30bac772da2
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-10 23:18:58 +0200

    Create full Filters sub-module components

---
+++ src/components/Filters/FiltersBar/FiltersBar.jsx
@@ -0,0 +1,25 @@
+import './FiltersBar.css'
+
+function FiltersBar({ search, onSearchChange, house, onHouseChange, role, onRoleChange }) {
+    return (
+        <div className="filtersBar">
+            <SearchBar value={search} onChange={onSearchChange} />
+            <FilterSelect
+                id="houseFilter"
+                label="Casa"
+                options={HOUSES}
+                value={house}
+                onChange={onHouseChange}
+            />
+            <FilterSelect
+                id="roleFilter"
+                label="Rol en Hogwarts"
+                options={ROLES}
+                value={role}
+                onChange={onRoleChange}
+            />
+        </div>
+    )
+}
+
+export default FiltersBar
