553aedd222213ed6cbef7617b418df72b3fbf733
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-10 22:50:40 +0200

    Add CharacterList component

---
+++ src/components/CharacterList/CharacterList.jsx
@@ -0,0 +1,23 @@
+import CharacterCard from "../CharacterCard/CharacterCard";
+import './CharacterList.css'
+
+function CharacterList({ characters }) {
+    if (characters.length === 0) {
+        return (
+            <p className="characterListEmpty">
+                No hay personajes que coincidan con tu búsqueda. Prueba a cambiar
+                los filtros.
+            </p>
+        )
+    }
+
+    return (
+        <div className="characterList">
+            {characters.map((character) => (
+                <CharacterCard key={character.id} character={character} />
+            ))}
+        </div>
+    )
+}
+
+export default CharacterList
