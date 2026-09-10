53c671099a3ca1844922a65388fbee6d272ffbc8
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-10 18:59:10 +0200

    chore: initial structure and project installations

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <h1>Vamos a empezar</h1>
+      <Header />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <h2>Este es mi header 👋</h2>
+  )
+}
+
+export default Header;
