189d507bccc19f2cb25156af858e5580819315a0
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-10 19:46:24 +0200

    feat(header): create main navigation bar and setup global styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <>
-      <h1>Vamos a empezar</h1>
       <Header />
+      <main>
+        <h1>Vamos a empezar</h1>
+      </main>
+      
     </>
   )
 }
