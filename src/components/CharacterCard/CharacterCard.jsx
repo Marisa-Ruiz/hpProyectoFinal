a302541e13129b618883c5656ffa34eee6faead4
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-10 21:59:55 +0200

    Add CharacterCard component

---
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -0,0 +1,56 @@
+import './CharacterCard.css'
+
+function houseClassName(house) {
+    if (!house) return ''
+    const formattedHouse = house.charAt(0).toUpperCase() + house.slice(1).toLowerCase()
+    return `card${formattedHouse}`
+}
+
+function CharacterCard({ character }) {
+    const { name, image, house, species, patronus, actor } = character
+
+    return (
+        <article className={`card ${houseClassName(house)}`}>
+            <div className="cardImageWrap">
+                <img
+                    className="cardImage"
+                    src={image || FALLBACK_IMAGE}
+                    alt={`Retrato de ${name}`}
+                    loading="lazy"
+                    onError={(event) => {
+                        event.currentTarget.src = FALLBACK_IMAGE
+                    }}
+                />
+            </div>
+
+            <div className="cardBody">
+                <h3 className="cardName">{name}</h3>
+
+                {house && <p className="cardHouse">{house}</p>}
+
+                <dl className="cardDetails">
+                    {species && (
+                        <div className="cardDetail">
+                            <dt>Especie</dt>
+                            <dd>{species}</dd>
+                        </div>
+                    )}
+                    {patronus && (
+                        <div className="cardDetail">
+                            <dt>Patronus</dt>
+                            <dd>{patronus}</dd>
+                        </div>
+                    )}
+                    {actor && (
+                        <div className="cardDetail">
+                            <dt>Actor / actriz</dt>
+                            <dd>{actor}</dd>
+                        </div>
+                    )}
+                </dl>
+            </div>
+        </article>
+    )
+}
+
+export default CharacterCard
